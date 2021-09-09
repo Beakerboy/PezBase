a5d132ba8da356751e99015bcc45f62610196d3f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-09-09 10:06:26 -0400

    Create Families.php

---
+++ pezbase_import/src/Plugin/migrate/source/Families.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\pezbase_import\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Minimalistic example for a SqlBase source plugin.
+ *
+ * @MigrateSource(
+ *   id = "taxonomy",
+ *   source_module = "pezbase_import",
+ * )
+ */
+class Taxonomy extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Source data is queried from 'families' table.
+    $query = $this->select('families', 'f')
+      ->fields('f', [
+          'family_id',
+          'family_name',
+          'family_predecessor',
+        ]);
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'tid'    => $this->t('ID'),
+      'name'   => $this->t('Name' ),
+      'parent' => $this->t('Parent'),
+    ];
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'family_id' => [
+        'type' => 'integer',
+        'alias' => 'f',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $row->setSourceProperty('vid', 'dispenser_family');
+    return parent::prepareRow($row);
+  }
+}
