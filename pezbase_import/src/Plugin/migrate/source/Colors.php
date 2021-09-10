a64cc6eff45ac8d04eb2d3184b1c91acc4c306d1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-09-10 12:25:41 -0400

    Update Colors.php

--- pezbase_import/src/Plugin/migrate/source/Colors.php
+++ pezbase_import/src/Plugin/migrate/source/Colors.php
@@ -1,1 +1,54 @@
+<?php
 
+namespace Drupal\pezbase_import\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Minimalistic example for a SqlBase source plugin.
+ *
+ * @MigrateSource(
+ *   id = "patent_numbers",
+ *   source_module = "pezbase_import",
+ * )
+ */
+class Colors extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Source data is queried from 'families' table.
+    $query = $this->select('colors', 'c')
+      ->fields('c', [
+          'color_id',
+          'color_name',
+        ]);
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'color_id'   => $this->t('ID'),
+      'color_name' => $this->t('Name' ),
+    ];
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'color_id' => [
+        'type' => 'integer',
+        'alias' => 'c',
+      ],
+    ];
+  }
+}
