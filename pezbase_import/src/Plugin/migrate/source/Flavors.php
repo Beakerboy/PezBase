ca61e2cd1079f675f24ae9d91517772cee6bafca
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-09-10 12:28:49 -0400

    Update Flavors.php

--- pezbase_import/src/Plugin/migrate/source/Flavors.php
+++ pezbase_import/src/Plugin/migrate/source/Flavors.php
@@ -1,1 +1,53 @@
+<?php
 
+namespace Drupal\pezbase_import\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Minimalistic example for a SqlBase source plugin.
+ *
+ * @MigrateSource(
+ *   id = "flavors",
+ *   source_module = "pezbase_import",
+ * )
+ */
+class Flavors extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('flavors', 'f')
+      ->fields('f', [
+          'flavor_id',
+          'flavor_name',
+        ]);
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'flavor_id'   => $this->t('ID'),
+      'flavor_name' => $this->t('Name' ),
+    ];
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'flavor_id' => [
+        'type' => 'integer',
+        'alias' => 'f',
+      ],
+    ];
+  }
+}
