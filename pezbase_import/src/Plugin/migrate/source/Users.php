1f2cf7ec782621038619dc8ccbef96b421b0407d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-09-11 08:30:59 -0400

    Update Users.php

--- pezbase_import/src/Plugin/migrate/source/Users.php
+++ pezbase_import/src/Plugin/migrate/source/Users.php
@@ -1,1 +1,64 @@
+<?php
 
+namespace Drupal\pezbase_import\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Minimalistic example for a SqlBase source plugin.
+ *
+ * @MigrateSource(
+ *   id = "pezbase_users",
+ *   source_module = "pezbase_import",
+ * )
+ */
+class Users extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    select * from users where username !='pezadmin' AND username!='' order by user_id";
+    $query = $this->select('users', 'u')
+      ->fields('u', [
+          'user_id',
+          'username',
+          'crypt',
+          'email_address',
+          'created',
+          'last_access',
+        ])
+      ->condition('username', '', '<>')
+      ->condition('username', 'pezadmin', '<>');
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'user_id' => $this->t('ID'),
+      'username' => $this->t('Username'),
+      'crypt'   => $this->t('Password Hash'),
+      'email_address' => $this->t('Email Address' ),
+      'created' => $this->t('Created'),
+      'last_access' => $this->t('Last Access'),
+    ];
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'user_id' => [
+        'type' => 'integer',
+        'alias' => 'u',
+      ],
+    ];
+  }
+}
