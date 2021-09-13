c7399862b20e370ff4e2a73df0901d4a01d23182
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-09-13 17:56:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
+git clone git@github.com:Beakerboy/Pezbase
 composer create-project drupal/recommended-project pezbase
 cd pezbase
 composer require drush/drush
 vendor/bin/drush site-install --db-url=mysql://pezadmin:password@localhost/pezbase
+cd modules
+ln -s ../../Pezbase/pezbase_data
+ln -s ../../Pezbase/pezbase_examples
+
