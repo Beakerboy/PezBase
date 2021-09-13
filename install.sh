ed36027e708a0e2089d247d0e54ec25b5e1bf80e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-09-13 18:53:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 mysql -u root -p -e "CREATE DATABASE pezbase"
 mysql -u root -p -e "CREATE USER pezbase@localhost IDENTIFIED BY 'password';"
 mysql -u root -p -e "grant all privileges on pezbase.* to pezbase@localhost;"
-
 git clone git@github.com:Beakerboy/Pezbase
 composer create-project drupal/recommended-project pezbase
 cd pezbase
 composer require drush/drush
 vendor/bin/drush site-install --db-url=mysql://pezadmin:password@localhost/pezbase
-cd web/modules
-ln -s ../../../Pezbase/pezbase_data
-ln -s ../../../Pezbase/pezbase_examples
+ln -s ../../../Pezbase/pezbase_data web/modules
+ln -s ../../../Pezbase/pezbase_examples web/modules
+vendor/bin/drush en pezbase_examples
 
