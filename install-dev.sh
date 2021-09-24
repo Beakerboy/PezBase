caeb5415d18964d25c7a1276bdaf1dc126623d30
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-09-24 09:38:08 -0400

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -7,4 +7,4 @@ cd pezbase
 composer install
 vendor/bin/drush site-install --db-url=mysql://pezbase:password@localhost/pezbase --site-name PezBase --account-pass password
 sudo chmod -R 777 web/sites/default/files/
-vendor/bin/drush en pezbase_examples
+vendor/bin/drush en pezbase_examples pezbase_ui
